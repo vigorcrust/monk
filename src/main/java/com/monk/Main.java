9182c428033844fce5fa1feb3ac750992051ea72
alha96 <hatzold.alexander@gmail.com>
2017-06-08 13:03:41 +0200

    test commit

--- src/main/java/com/monk/Main.java
+++ src/main/java/com/monk/Main.java
@@ -3,6 +3,6 @@ package com.monk;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("first change");
+        System.out.println("first change 1");
     }
 }
