d7f60675ce13878b4cbe41b0ff6b7f29d7d42743
alha96 <info24816@gmail.com>
2017-06-08 15:07:48 +0200

    added config.json to resources

--- src/main/java/com/monk/Main.java
+++ src/main/java/com/monk/Main.java
@@ -1,8 +1,17 @@
 package com.monk;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigObject;
+import com.typesafe.config.ConfigValue;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("first change 2");
+        Config config = ConfigFactory.load("config");
+        ConfigObject obj = config.root();
+        for (String s : obj.keySet()) {
+            System.out.println(s);
+        }
     }
 }
