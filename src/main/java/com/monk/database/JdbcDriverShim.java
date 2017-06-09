3ac761bb0737469819f3d0466925895bd1e2bbb0
vigorcrust <ddmartens@gmail.com>
2017-06-09 20:18:22 +0200

    Extracted config from documentation to example config.json file. Added example log4j.properties file. Added first draft of jdbc DriverShim to create a facade.

---
+++ src/main/java/com/monk/database/JdbcDriverShim.java
@@ -0,0 +1,41 @@
+package net.adoris.database;
+
+import java.sql.*;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class JdbcDriverShim implements Driver{
+    private Driver driver;
+
+    public JdbcDriverShim(Driver driver) {
+        this.driver = driver;
+    }
+
+    public Connection connect(String s, Properties properties) throws SQLException {
+        return this.driver.connect(s, properties);
+    }
+
+    public boolean acceptsURL(String s) throws SQLException {
+        return this.driver.acceptsURL(s);
+    }
+
+    public DriverPropertyInfo[] getPropertyInfo(String s, Properties properties) throws SQLException {
+        return this.driver.getPropertyInfo(s, properties);
+    }
+
+    public int getMajorVersion() {
+        return this.driver.getMajorVersion();
+    }
+
+    public int getMinorVersion() {
+        return this.driver.getMinorVersion();
+    }
+
+    public boolean jdbcCompliant() {
+        return this.driver.jdbcCompliant();
+    }
+
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return this.driver.getParentLogger();
+    }
+}
